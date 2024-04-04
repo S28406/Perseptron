7fe6e4438c487003dfcd3832683250159d004eeb
S28406 <s28406@pjwstk.edu.pl>
2024-04-04 19:17:42 +0200

    added read method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        String filename = "iris.test.data";
+    static LinkedHashMap<ArrayList<Double>, String> trainingMap;
+    static LinkedHashMap<ArrayList<Double>, String> testMap;
+    public static void main(String[] args) throws IOException {
+        File trainingFile = new File("perceptron.data");
+        trainingMap = new LinkedHashMap<>(read(trainingFile));
+        File testFile = new File("perceptron.test.data");
+        testMap = new LinkedHashMap<>(read(testFile));
+        System.out.println("flag");
         Perceptron perceptron = new Perceptron();
+    }
+    public static Map<ArrayList<Double>, String> read(File file) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        HashMap<ArrayList<Double>, String> results = new HashMap<>();
+        String line;
+        while((line = br.readLine()) != null){
+            ArrayList<Double> coords = new ArrayList<>();
+            ArrayList<String> allData = new ArrayList<>(Arrays.asList(line.split(",")));
+            for(int i = 0; i < allData.size()-1; i++){
+                coords.add(Double.parseDouble(allData.get(i)));
+            }
+            results.put(coords, allData.get(allData.size()-1));
+        }
+        br.close();
+        return results;
     }
 }
--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+import java.util.Map;
+
 class Perceptron {
-    private double weight;
-    private double theta = Math.random() * 5;
+    Map.Entry<ArrayList<Double>, String> mapEntry = Main.trainingMap.entrySet().iterator().next();
+    private double numWeight = mapEntry.getKey().size();
+    private ArrayList<Double> weights = new ArrayList<>();
+    private double theta = Math.random();
     private double alpha = 0.4;
 
 
     public Perceptron() {
-        this.weight = Math.random();
+        for(int i = 0; i <  numWeight; i++){
+            double weight = Math.random();
+            weights.add(weight);
+        }
     }
 
     public int predict(double[] inputs) {
