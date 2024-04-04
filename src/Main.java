e43079a91fa9ac09bf6ae167181ee1da132446d7
S28406 <s28406@pjwstk.edu.pl>
2024-04-04 17:49:30 +0200

    Created Perseptron.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String filename = "iris.test.data";
+        Perceptron perceptron = new Perceptron();
     }
 }
---
+++ src/Perceptron.java
@@ -0,0 +1,14 @@
+class Perceptron {
+    private double weight;
+    private double theta = Math.random() * 5;
+    private double alpha = 0.4;
+
+
+    public Perceptron() {
+        this.weight = Math.random();
+    }
+
+    public int predict(double[] inputs) {
+        return 0;
+    }
+}
